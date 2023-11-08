2a1a1afae8efbbd3c8a75983b9ccb22b0a3e1e10
004 <anbschool0004@004ui-Macmini.local>
2023-11-08 14:10:34 +0700

    md2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Drawer from './Component/Drawer'
+import TopBar from './Component/TopBar'
+
+function App() {
+
+return (
+  <>
+    <TopBar />
+    <Drawer />
+  </>
+)
+}
+
+export default App
